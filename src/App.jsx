765a94352928374b0af8253a21168dc14a1e1606
markopaunovic1 <marpau970514@hotmail.com>
2023-10-09 18:52:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './components/Counter'
+import Profile from './components/Profile'
+import { useSelector } from 'react-redux'
+
+function App() {
+  const alias = useSelector(state => state.profile.alias);
+
+  
+  return (
+    <div>
+      <h2> Välkommen, {alias}</h2>
+      <Profile />
+
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+      
+  )
+}
+
+export default App
